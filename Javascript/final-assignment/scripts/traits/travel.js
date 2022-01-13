ada543c753b5e6ae7a6f2247c6f5040609020d6a
pukarsubedi01 <pukars57@gmail.com>
2022-01-13 20:41:00 +0545

    Bullet travel bug fix

--- Javascript/final-assignment/scripts/traits/travel.js
+++ Javascript/final-assignment/scripts/traits/travel.js
@@ -5,20 +5,22 @@ export default class Travel extends Trait {
     this.direction = 1;
     this.initialBulletPosX = 0;
     this.firingRange = 0;
-    this.maxTravelDistance = 0;
+    this.maxTravelDistanceRight = 0;
+    this.maxTravelDistanceLeft = 0;
   }
 
   update(entity, deltaTime, level) {
-    this.maxTravelDistance = this.initialBulletPosX + this.firingRange;
+    this.maxTravelDistanceRight = this.initialBulletPosX + this.firingRange;
+    this.maxTravelDistanceLeft = this.initialBulletPosX - this.firingRange;
     entity.pos.x += entity.vel.x * deltaTime * this.direction;
 
     entity.pos.y += entity.vel.y * deltaTime;
-
-    if (this.direction === 1 && entity.pos.x > this.maxTravelDistance) {
+    console.log(this.maxTravelDistance, this.initialBulletPosX);
+    if (this.direction === 1 && entity.pos.x > this.maxTravelDistanceRight) {
       level.entities.delete(entity);
     } else if (
       this.direction === -1 &&
-      entity.pos.x < -this.maxTravelDistance
+      entity.pos.x < this.maxTravelDistanceLeft
     ) {
       level.entities.delete(entity);
     }
